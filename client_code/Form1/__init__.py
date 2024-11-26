a9f90764b167a570500bfc89b3bce413eff83616
walternzd@gmail.com <walternzd@gmail.com>
2024-11-26 07:51:25 +0000

    Created ServerModule1 and LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
@@ -8,3 +12,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def submit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,27 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+from datetime import datetime
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def add_feedack(name, email, feedback):
+  app_tables.feedback.add_row(
+    name=name,
+    email=email,
+    feedback=feedback,
+    created_on=datetime.now())
+  submit_button_click
